29a2ea8b22b0ec1245a364de7e087e0ed6495709
jogendra.singh <Diya@2595>
2023-11-28 10:32:34 -0500

    post changes it is working

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -10,14 +10,14 @@ API_PREFIX = "/api"
 
 SECRET_KEY = config("SECRET_KEY", cast=Secret, default="CHANGEME")
 
-POSTGRES_USER = config("POSTGRES_USER", cast=str)
-POSTGRES_PASSWORD = config("POSTGRES_PASSWORD", cast=Secret)
+POSTGRES_USER = config("POSTGRES_USER", cast=str, default="postgres")
+POSTGRES_PASSWORD = config("POSTGRES_PASSWORD", cast=Secret, default="postgres")
 POSTGRES_SERVER = config("POSTGRES_SERVER", cast=str, default="db")
 POSTGRES_PORT = config("POSTGRES_PORT", cast=str, default=5432)
-POSTGRES_DB = config("POSTGRES_DB", cast=str)
+POSTGRES_DB = config("POSTGRES_DB", cast=str, default="postgres")
 
 DATABASE_URL = config(
     "DATABASE_URL",
-    cast= DatabaseURL,
+    cast=DatabaseURL,
     default=f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
 )
---
+++ backend/app/db/migrations/versions/d3a525f3eab2_create_main_tables.py
@@ -0,0 +1,34 @@
+"""create_main_tables
+
+Revision ID: d3a525f3eab2
+Revises: 
+Create Date: 2023-11-28 12:08:54.921266
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic
+revision = 'd3a525f3eab2'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def create_cleanings_table() -> None:
+    op.create_table(
+        "cleanings",
+        sa.Column("id", sa.Integer, primary_key=True),
+        sa.Column("name", sa.Text, nullable=False, index=True),
+        sa.Column("description", sa.Text, nullable=True),
+        sa.Column("cleaning_type", sa.Text, nullable=False, server_default="spot_clean"),
+        sa.Column("price", sa.Numeric(10, 2), nullable=False),
+    )
+
+
+def upgrade() -> None:
+    create_cleanings_table()
+
+
+def downgrade() -> None:
+    op.drop_table("cleanings")
